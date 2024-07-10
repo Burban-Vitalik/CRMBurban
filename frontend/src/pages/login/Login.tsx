b87818d17d766dae8d7205ba42e07aaab2828170
Volyarosh94 <vol.yarosh.94@gmail.com>
2024-07-10 20:23:33 +0300

    make better view of forms using MUI

--- frontend/src/pages/login/Login.tsx
+++ frontend/src/pages/login/Login.tsx
@@ -1,12 +1,27 @@
 import React from "react";
 import { LoginForm } from "./LoginForm";
+import { Box, Container, Paper, Typography } from "@mui/material";
+import { Link } from "react-router-dom";
 
 interface ILoginProps {}
 
 export const Login: React.FC<ILoginProps> = () => {
   return (
-    <div>
-      <LoginForm />
-    </div>
+    <Container maxWidth="sm">
+      <Paper elevation={3} sx={{ padding: 3, marginTop: 8 }}>
+        <Typography variant="h4" component="h1" gutterBottom>
+          Login
+        </Typography>
+        <Box sx={{ marginTop: 2 }}>
+          <LoginForm />
+        </Box>
+        <Box sx={{ marginTop: 2, textAlign: "center" }}>
+          <Typography variant="body2">
+            Don't have an account?
+            <Link to="/register">Register</Link>
+          </Typography>
+        </Box>
+      </Paper>
+    </Container>
   );
 };
--- frontend/src/pages/register/Register.tsx
+++ frontend/src/pages/register/Register.tsx
@@ -1,12 +1,27 @@
 import React from "react";
 import { RegisterForm } from "./RegisterForm";
+import { Box, Container, Paper, Typography } from "@mui/material";
+import { Link } from "react-router-dom";
 
 interface IRegisterProps {}
 
 export const Register: React.FC<IRegisterProps> = () => {
   return (
-    <div>
-      <RegisterForm />
-    </div>
+    <Container maxWidth="sm">
+      <Paper elevation={3} sx={{ padding: 3, marginTop: 8 }}>
+        <Typography variant="h4" component="h1" gutterBottom>
+          Register
+        </Typography>
+        <Box sx={{ marginTop: 2 }}>
+          <RegisterForm />
+        </Box>
+        <Box sx={{ marginTop: 2, textAlign: "center" }}>
+          <Typography variant="body2">
+            Already have an account?
+            <Link to="/login">Login</Link>
+          </Typography>
+        </Box>
+      </Paper>
+    </Container>
   );
 };
